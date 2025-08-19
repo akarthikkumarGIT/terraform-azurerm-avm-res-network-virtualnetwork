9a840981810d5bff220de6f2d77d74a72538e307
akarthikkumarGIT <akarthikkumar@gmail.com>
2025-08-19 16:19:32 -0700

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,7 @@
+     provider "azurerm" {
+      features {}
+      client_id = "a4c35d0f-faa7-4046-a856-ac0fc11824da"
+      client_secret = "z5I8Q~Nf5fso3ofqoj3u.CmdTy8FDZpN9oEXEbC_"
+      tenant_id = "5e4c6417-3498-4f00-8af0-2720529fcfb1"
+      subscription_id = "11da9e6a-2de9-41d7-b691-16e4d50865a9"
+    }
